0ab8fd6d3bd4113209810ddc254e3bd9e8c989f7
tmsmendez <31008721+tmsmendez@users.noreply.github.com>
2025-07-04 17:31:39 +0000

    Deploying to gh-pages from @ tmsmendez/tmsmendez.github.io@4384c31b6335c49694d34c99edf3214734eb79e8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -149,7 +149,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%6D%65%6E%64%65%7A%65@%75%77.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -159,25 +159,18 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/tomas-mendez-echenagucia", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=OyDWXZwAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
