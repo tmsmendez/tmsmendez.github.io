a208c5a9f73726a8e0223586b5ec7d5990d61c5e
tmsmendez <31008721+tmsmendez@users.noreply.github.com>
2025-07-05 04:19:53 +0000

    Deploying to gh-pages from @ tmsmendez/tmsmendez.github.io@15a125ed6684743234e5db52dfa5f089e7191029 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "Research",
-          description: "Selected research....",
+          description: "Current and past research projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
